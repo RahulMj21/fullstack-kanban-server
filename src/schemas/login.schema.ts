239e5d07b9c6ee1b3443f00786a11504d73da675
Rahul Mondal <rahulmondar@gmail.com>
2022-12-25 13:34:18 +0530

    refactor: modified login and register schema

--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -5,7 +5,9 @@ export const LoginSchema = object({
         email: string({ required_error: "please provide email" }).email(
             "invalid email"
         ),
-        password: string({ required_error: "password is required" }),
+        password: string({ required_error: "password is required" })
+            .min(8, "password must contain atleast 8 characters")
+            .max(20, "password must contain less than 20 characters"),
     }),
 });
 
--- src/schemas/register.schema.ts
+++ src/schemas/register.schema.ts
@@ -8,10 +8,14 @@ export const RegisterSchema = object({
         email: string({ required_error: "please provide email" }).email(
             "invalid email"
         ),
-        password: string({ required_error: "password is required" }),
+        password: string({ required_error: "password is required" })
+            .min(8, "password must contain atleast 8 characters")
+            .max(20, "password must contain less than 20 characters"),
         confirmPassword: string({
             required_error: "confirmPassword is required",
-        }),
+        })
+            .min(8, "confirm password must contain atleast 8 characters")
+            .max(20, "confirm password must contain less than 20 characters"),
     }).refine((data) => data.password === data.confirmPassword, {
         message: "confirm password dosen't matches with the password",
         path: ["confirmPassword"],
