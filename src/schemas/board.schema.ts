c00abc5f5fbcb64fa4047271d593e2d72c837f07
Rahul Mondal <rahulmondar@gmail.com>
2023-02-27 10:38:21 +0530

    fix: board.schema.ts file

--- src/schemas/board.schema.ts
+++ src/schemas/board.schema.ts
@@ -5,7 +5,7 @@ export const BoardSchema = object({
         title: string({ required_error: "please provide board title" })
             .trim()
             .min(3, "title must contain atleast 3 chracters")
-            .max(4, "title must contain less than 20 characters"),
+            .max(20, "title must contain less than 20 characters"),
         description: string()
             .trim()
             .max(500, "description must contain less than 500 characters")
